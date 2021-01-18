80fe84a6ad7df3416333695e97430407e9d5dce4
hyunss <khh3599@naver.com>
2021-01-18 19:06:38 +0900

    [Share] viewpager와 tablayout 연결

--- app/src/main/java/com/nexters/sticky/ui/share/ShareActivity.kt
+++ app/src/main/java/com/nexters/sticky/ui/share/ShareActivity.kt
@@ -2,6 +2,8 @@ package com.nexters.sticky.ui.share
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nexters.sticky.R
 import com.nexters.sticky.base.BaseActivity
 import com.nexters.sticky.databinding.ActivityShareBinding
@@ -22,5 +24,10 @@ class ShareActivity : BaseActivity<ActivityShareBinding>() {
 
 		binding.shareViewPager.adapter =  ShareFragmentAdapter(this)
 
+		val tabLayoutTextArray = arrayOf("현재 기록","누적 기록", "최근 뱃지")
+
+		TabLayoutMediator(binding.shareTabLayout, binding.shareViewPager){tab,position ->
+			tab.text = tabLayoutTextArray[position]
+		}.attach()
 	}
 }
