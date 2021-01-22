1c2a44c4df46237933704d8cd51c94eb2a6404ae
YChaeeun <ychae.leah@gmail.com>
2021-01-23 00:11:45 +0900

    [splash] UI 작성

--- app/src/main/java/com/nexters/sticky/utils/ViewExtension.kt
+++ app/src/main/java/com/nexters/sticky/utils/ViewExtension.kt
@@ -8,3 +8,13 @@ internal fun View.hideKeyboard() {
 	val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 	imm.hideSoftInputFromWindow(windowToken, 0)
 }
+
+fun View.showAnimation() {
+	alpha = 0.0F
+
+	animate()
+		.translationY(height.toFloat())
+		.alpha(1.0f)
+		.setStartDelay(1000L)
+		.setListener(null).duration = 1000L
+}
