965f2bd84c15ad516ff9c39cbed67f072974d097
YChaeeun <ychae.leah@gmail.com>
2021-02-15 00:47:28 +0900

    [공통] Glide 적용

--- app/src/main/java/com/nexters/sticky/utils/ViewExtension.kt
+++ app/src/main/java/com/nexters/sticky/utils/ViewExtension.kt
@@ -1,12 +1,21 @@
 package com.nexters.sticky.utils
 
 import android.content.Context
+import android.util.TypedValue
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 internal fun View.hideKeyboard() {
 	val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 	imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun View.convertFloatToDp(value: Float): Int {
+	return TypedValue.applyDimension(
+		TypedValue.COMPLEX_UNIT_DIP,
+		value,
+		resources.displayMetrics
+	).toInt()
 }
 
 fun View.showAnimation() {
