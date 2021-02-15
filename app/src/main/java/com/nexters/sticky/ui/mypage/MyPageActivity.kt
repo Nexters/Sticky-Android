32471f33284ccda4c4d1c60c4b0d94e8d20ce869
YChaeeun <ychae.leah@gmail.com>
2021-02-16 00:57:07 +0900

    [myPage] 2차 디자인 반영

--- app/src/main/java/com/nexters/sticky/ui/mypage/MyPageActivity.kt
+++ app/src/main/java/com/nexters/sticky/ui/mypage/MyPageActivity.kt
@@ -15,7 +15,7 @@ class MyPageActivity : BaseActivity<ActivityMyPageBinding>() {
 	override val viewModel: MyPageViewModel by viewModels()
 
 	override val layoutRes = R.layout.activity_my_page
-	override val actionBarLayoutRes = R.layout.actionbar_my_page_layout
+	override val actionBarLayoutRes = R.layout.actionbar_main_layout
 	override val statusBarColorRes = R.color.primary_white
 
 	override fun setUpBinding() {
@@ -32,10 +32,13 @@ class MyPageActivity : BaseActivity<ActivityMyPageBinding>() {
 
 	private fun setActionBar() {
 		actionBar.setText(R.id.tv_title, getString(R.string.my_page_title))
+
+		actionBar.setImage(R.id.btn_left, R.drawable.ic_icon_close)
 		actionBar.clickListener(R.id.btn_left) {
 			finish()
 		}
 
+		actionBar.setImage(R.id.btn_right, R.drawable.ic_icon_more)
 		actionBar.clickListener(R.id.btn_right) {
 			val intent = Intent(this@MyPageActivity, ViewMoreActivity::class.java)
 			startActivity(intent)
