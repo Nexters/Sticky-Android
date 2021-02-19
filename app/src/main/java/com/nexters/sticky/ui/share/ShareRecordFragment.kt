3d9bb1e8b62cf16b7ae9f8dc59c01222f1d2e5b8
hyunss <khh3599@naver.com>
2021-02-20 05:05:58 +0900

    [Share] Glide 및 layout 디자인 적용

--- app/src/main/java/com/nexters/sticky/ui/share/ShareRecordFragment.kt
+++ app/src/main/java/com/nexters/sticky/ui/share/ShareRecordFragment.kt
@@ -1,12 +1,19 @@
 package com.nexters.sticky.ui.share
 
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.nexters.sticky.R
 import com.nexters.sticky.base.BaseFragment
 import com.nexters.sticky.databinding.FragmentShareRecordBinding
+import com.nexters.sticky.utils.convertFloatToDp
 import java.io.File
 import java.io.FileOutputStream
 
@@ -21,6 +28,7 @@ class ShareRecordFragment : BaseFragment<FragmentShareRecordBinding>() {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+		setBackground()
 		getCaptureScreen()
 	}
 
@@ -41,7 +49,21 @@ class ShareRecordFragment : BaseFragment<FragmentShareRecordBinding>() {
 				e.printStackTrace()
 			}
 		}
+	}
 
+	private fun setBackground() {
+		val roundedCorners = binding.root.convertFloatToDp(16F)
 
+		Glide.with(this@ShareRecordFragment)
+			.load(R.drawable.sharebackground_level01)
+			.transform(CenterCrop(), RoundedCorners(roundedCorners))
+			.into(object : CustomTarget<Drawable>() {
+				override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable?>?) {
+					binding.captureLayoutRecord.background = resource
+				}
+
+				override fun onLoadCleared(placeholder: Drawable?) {
+				}
+			})
 	}
 }
